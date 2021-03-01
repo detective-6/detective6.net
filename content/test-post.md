5a67b065029d8b3042d8d13f776c8a6b1bd5aec4
Brandon Gnash <brandon.gnash@gmail.com>
2021-02-28 20:53:03 -0800

    Initial commit

---
+++ content/test-post.md
@@ -0,0 +1,5 @@
+Title: Test Post
+Date: 2021-02-28 19:40
+Category: programming
+
+This is a test post to set up my new blog.
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+# Theme customizations
+THEME = '/home/detective6/Code/pelican-themes/MinimalXY'
+MINIMALXY_FAVICON = 'favicon.ico'
+SITENAME = 'Detective 6'
+SITEURL = ''
+AUTHOR = 'Brandon Gnash'
+AUTHOR_AVATAR = 'https://s.gravatar.com/avatar/ebc6eb739ccfc88592981acc6a15ff97?s=240'
+AUTHOR_INTRO = "Howdy! I'm Gnash. I like security, puzzles, music, and horror movies."
+
+PATH = 'content'
+TIMEZONE = 'America/Los_Angeles'
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (
+	('twitter', 'https://twitter.com/detective_6'),
+        ('instagram', 'https://www.instagram.com/detective_6/'),
+	('github', 'https://github.com/detective-6')
+)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
